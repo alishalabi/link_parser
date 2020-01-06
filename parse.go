3adf4d57b6625b2ffb4a07f41c758baeb90cd049
Ali Shalabi <ashalabi13@gmail.com>
2020-01-06 13:52:17 -0800

    Testing dfs

--- parse.go
+++ parse.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"io"
+	"fmt"
 )
 
 // Link represents all the data available in an html link
@@ -12,5 +13,17 @@ type Link struct {
 
 // Parse will take HTML as input, returns slice of links as output
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"   ")
+	}
 }
